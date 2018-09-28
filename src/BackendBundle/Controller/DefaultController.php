3b7f1f039a6dc2efb84fc02bdc45c78e330e4254
Dani_crep <dneli6cres@outlook.com>
2018-09-28 16:20:02 -0600

    Probar una entidad (modelo y repositorio)

--- src/BackendBundle/Controller/DefaultController.php
+++ src/BackendBundle/Controller/DefaultController.php
@@ -8,6 +8,14 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();//accede a las entidades mediante doctrine
+        $user_repo = $em->getRepository("BackendBundle:User");//respositorio que tiene un modelo de cosulta
+        
+        $user = $user_repo->find(1); //obteneel primer usuario
+        echo"Bienvenido".$user->getName()." ".$user->getSurname(); //muestra en un strim el nombre con el apéllido de usuario obtenido
+        var_dump($user);//para ver los que genera la cosulta
+        die();//paar que synfony no pida una vista
+        
         return $this->render('BackendBundle:Default:index.html.twig');
     }
 }
