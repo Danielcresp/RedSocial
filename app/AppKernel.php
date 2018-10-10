a3e72b5ec85744b0dbae84aba5aa34061320e59c
Dani_crep <dneli6cres@outlook.com>
2018-10-10 09:07:13 -0600

    Configurar un sistema de paginaciones

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(), /*agregar la paginacion para usarla*/
             new AppBundle\AppBundle(),
             new BackendBundle\BackendBundle(),
         ];
