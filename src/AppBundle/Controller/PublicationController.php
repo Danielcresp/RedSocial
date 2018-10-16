01a0fad91cca99de40cdd3b52019acae6f5ec09c
Dani_crep <dneli6cres@outlook.com>
2018-10-15 21:31:17 -0600

    Crear formulario de publicaciones Nuevo nombre

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -6,9 +6,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use BackendBundle\Entity\Publication;
+use AppBundle\Form\PublicationType;
+
 class PublicationController extends Controller{
-        public function indexAction (Request $request){
+
+    public function indexAction (Request $request){
+
+        $em = $this->getDoctrine()->getManager();
+		$user = $this->getUser();
+		
+		$publication = new Publication();
+		$form = $this->createForm(PublicationType::class, $publication);
         
-        return $this->render('AppBundle:Publication:home.html.twig');
+        return $this->render('AppBundle:Publication:home.html.twig',array(
+			'form' => $form->createView(),
+		));
     }
 }
