497258871826e604ca33f4901d67087923a5f6a1
Dani_crep <dneli6cres@outlook.com>
2018-10-15 22:02:03 -0600

    Añadir publicaciones

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -5,11 +5,18 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 use BackendBundle\Entity\Publication;
 use AppBundle\Form\PublicationType;
 
 class PublicationController extends Controller{
+	private $session;
+
+	public function __construct() {
+		$this->session = new Session();
+	}
 
     public function indexAction (Request $request){
 
@@ -18,7 +25,71 @@ class PublicationController extends Controller{
 		
 		$publication = new Publication();
 		$form = $this->createForm(PublicationType::class, $publication);
-        
+
+		$form->handleRequest($request);
+		if($form->isSubmitted()){ /*si el fomulario esta enviaodo*/ 
+			if($form->isValid()){ /*y es valido*/
+				
+				// upload image
+				$file = $form['image']->getData(); /*obtinen la imagen*/
+				
+				
+				if(!empty($file) && $file != null){
+					$ext = $file->guessExtension();
+				
+					if($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png' || $ext == 'gif'){
+						$file_name = $user->getId().time().".".$ext;
+						$file->move("uploads/publications/images", $file_name);
+						
+						$publication->setImage($file_name);
+					}else{
+						$publication->setImage(null);
+					}
+				}else{
+					$publication->setImage(null);
+				}
+				
+				// upload document
+				
+				$doc = $form['document']->getData();
+				
+				if(!empty($doc) && $doc != null){
+					$ext = $doc->guessExtension();
+					
+					if($ext == 'pdf'){
+						$file_name = $user->getId().time().".".$ext;
+						$doc->move("uploads/publications/documents", $file_name);
+						
+						$publication->setDocument($file_name);
+					}else{
+						$publication->setDocument(null);
+					}
+				}else{
+					$publication->setDocument(null);
+				}
+				
+				$publication->setUser($user);
+				$publication->setCreatedAt(new \DateTime("now"));
+				
+				$em->persist($publication);
+				$flush = $em->flush();
+				
+				if($flush == null){
+					$status = 'La publicación se ha creado correctamente !!';
+				}else{
+					$status = 'Error al añadir la publicación !!';
+				}
+				
+			}else{
+				$status = 'La publicación no se ha creado, porque el formulario no es válido !!';
+			}
+			
+			$this->session->getFlashBag()->add("status", $status);
+			return $this->redirectToRoute('home_publications');
+		}
+		
+/*		$publications = $this->getPublications($request);
+*/        
         return $this->render('AppBundle:Publication:home.html.twig',array(
 			'form' => $form->createView(),
 		));
